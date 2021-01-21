d294bf220edbab331ba29fb08ffd5fb2106356be
jiangpeng.jiang <jiangpeng.jiang@zhaopin.com.cn>
2021-01-21 20:44:37 +0800

    add FP-growth

---
+++ fp_growth/demo2/fp_growth.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import pyfpgrowth
+
+df = pd.DataFrame([
+    'A,B,E',
+    'B,D',
+    'B,C',
+    'A,B,D',
+    'A,C',
+    'B,C',
+    'A,C',
+    'A,B,C,E',
+    'A,B,C'
+])
+df.columns = ['items']
+df['items'] = df['items'].map(lambda x: x.split(','))
+
+print(df)
+
+res = []
+for i in df.values.tolist():
+    print(i[0])
+    res.append(i[0])
+
+
+
+patterns = pyfpgrowth.find_frequent_patterns(res, 2)
+print(patterns)
+
+rules = pyfpgrowth.generate_association_rules(patterns, 0.7)
+print(rules)
